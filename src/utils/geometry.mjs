6a8c826085bd7db9c21425b00172ade34c4adca4
Sergey Kuzmich <accounts@kuzmi.ch>
2024-10-20 23:48:10 +0200

    refactor: Fixing formatting and updating Pilorama desktop version to 5.0.0

--- src/utils/geometry.mjs
+++ src/utils/geometry.mjs
@@ -1,10 +1,10 @@
 export function mouseAngle(mousePoint, centerPoint) {
     const angle =
-                Math.atan(
-                    Math.abs(mousePoint.x - centerPoint.x) /
-                    Math.abs(mousePoint.y - centerPoint.y)
-                    )
-                * (180 / Math.PI);
+        Math.atan(
+            Math.abs(mousePoint.x - centerPoint.x) /
+            Math.abs(mousePoint.y - centerPoint.y)
+        )
+        * (180 / Math.PI);
 
     if (mousePoint.x >= centerPoint.x)
         return mousePoint.y <= centerPoint.y ? precision(angle) : 180 - precision(angle);
@@ -18,7 +18,7 @@ function modulo(num, denom) {
     return res >= 0 ? res : res + denom;
 }
 
-function precision(num){
+function precision(num) {
     var precision = 6
     var y = Math.abs(num) + precision / 2;
     y = y - y % precision;
@@ -27,7 +27,6 @@ function precision(num){
 
 
 export function lessDelta(newAngle, prevAngle) {
-
     const delta1 = modulo(newAngle - prevAngle, 360);
     const delta2 = modulo(prevAngle - newAngle, 360);
 
