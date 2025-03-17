5ff195f4ae47ad9f0c45cce992314920100d876e
Eugene Platonov <eugeneplatonoff@gmail.com>
2025-03-17 23:36:47 +0200

    Upgrade Qt version to 6.8.2 in workflows and script.

--- src/mac/bundle.sh
+++ src/mac/bundle.sh
@@ -3,7 +3,7 @@
 # This script is intended to be run as a "Run Script" build phase in Xcode.
 
 APP_BUNDLE_PATH="${BUILT_PRODUCTS_DIR}/${TARGET_NAME}.app"
-QT_BIN_DIR="${HOME}/bin/Qt/6.7.2/macos/bin"  # Adjust this if your Qt installation is in a different location
+QT_BIN_DIR="${HOME}/bin/Qt/6.8.2/macos/bin"  # Adjust this if your Qt installation is in a different location
 
 
 if [ "${CONFIGURATION}" == "Release" ] || [ "${CONFIGURATION}" == "MinSizeRel" ]; then
