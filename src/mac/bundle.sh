8d0642380d6730c143194123605363c48255f0fb
Eugene Platonov <eugeneplatonoff@gmail.com>
2025-06-30 01:17:04 +0300

    Allow Debug configuration in macOS bundle script

--- src/mac/bundle.sh
+++ src/mac/bundle.sh
@@ -6,7 +6,7 @@ APP_BUNDLE_PATH="${BUILT_PRODUCTS_DIR}/${TARGET_NAME}.app"
 QT_BIN_DIR="${HOME}/bin/Qt/6.9.1/macos/bin"  # Adjust this if your Qt installation is in a different location
 
 
-if [ "${CONFIGURATION}" == "Release" ] || [ "${CONFIGURATION}" == "MinSizeRel" ]; then
+if [ "${CONFIGURATION}" == "Release" ] || [ "${CONFIGURATION}" == "MinSizeRel" ] || [ "${CONFIGURATION}" == "Debug" ]; then
     "${QT_BIN_DIR}/macdeployqt" "$APP_BUNDLE_PATH" -qmldir="${SRCROOT}" -appstore-compliant  -codesign="$EXPANDED_CODE_SIGN_IDENTITY" -hardened-runtime
 
     find "$APP_BUNDLE_PATH/" -type d -name "*.dSYM" | while read dsym; do
